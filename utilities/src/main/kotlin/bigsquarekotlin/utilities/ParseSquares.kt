9a107ca9baa7bbf277a428faf2a2694d0536a4fb
Andy Smith <andyrsmith@gmail.com>
2024-01-20 00:19:03 +0000

    Validate arguments in init block

--- utilities/src/main/kotlin/bigsquarekotlin/utilities/ParseSquares.kt
+++ utilities/src/main/kotlin/bigsquarekotlin/utilities/ParseSquares.kt
@@ -5,7 +5,11 @@ import java.io.InputStream
 object Loader
 
 // NB - not generating equals or hashcode - caveat emptor!
-data class SquareArray( val size : Int, val ary : BooleanArray) {
+class SquareArray(val size : Int, private val ary : BooleanArray) {
+    init {
+        require(size * size == ary.size)  { "Arguments inconsistent size=${size} ary.size=${ary.size}"}
+    }
+
     private fun indexOf( y : Int, x : Int) = (y * size + x).also { idx ->
         require(idx in 0 until ary.size) { "Out of bounds!" }
     }
@@ -15,11 +19,15 @@ data class SquareArray( val size : Int, val ary : BooleanArray) {
     }
 
     operator fun get(y: Int, x: Int): Boolean = ary[indexOf(y,x)]
+
+    override fun toString(): String {
+        return "SquareArray(size=$size, ary (${ary.size}) =${ary.contentToString()})"
+    }
 }
 
-fun parseSquare(instream : InputStream) : SquareArray {
+fun parseSquare(inputStream : InputStream) : SquareArray {
     val lut = mapOf( ' ' to false, 'X' to true)
-    return instream
+    return inputStream
         .bufferedReader()
         .readLines()
         .map { it.trimEnd( '|')}
@@ -28,8 +36,7 @@ fun parseSquare(instream : InputStream) : SquareArray {
             require(sizes.size == 1) { "Uneven linesizes $sizes"}
             require(sizes.first() == lines.size )
             SquareArray( sizes.first(), lines.flatMap { line ->
-                 line.map { c -> lut[c] ?: error("Can't parse char $c") } }.toBooleanArray() ).also {
-                require( it.size * it.size == it.ary.size) {"Not a square array?!"} }
+                 line.map { c -> lut[c] ?: error("Can't parse char $c") } }.toBooleanArray() )
         }
 }
 
@@ -38,6 +45,9 @@ fun main() {
     val s = Loader::javaClass.javaClass.classLoader
     val instream = s.getResourceAsStream("dat1.txt") ?: error("Can't open file")
     val myAry = parseSquare(instream)
+
+    println(myAry)
+
     myAry[3,4] = false
     val bla = myAry[4,5]
 }
